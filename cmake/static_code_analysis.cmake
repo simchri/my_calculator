8b327b7371b3c4d06b48a1ccd6bc7e7cd633bb78
simchri <simon.massa@hotmail.de>
2024-12-31 12:03:55 +0100

    add cppcheck for apps and components

---
+++ cmake/static_code_analysis.cmake
@@ -0,0 +1,8 @@
+message(STATUS "Static code analysis tests setup ...")
+enable_testing()
+
+add_test(
+  NAME cppcheck
+  COMMAND cppcheck --error-exitcode=1 ${CMAKE_SOURCE_DIR}/apps ${CMAKE_SOURCE_DIR}/components
+)
+set_tests_properties(cppcheck PROPERTIES LABELS "sca;slow")
