d57b9fdf96685df794dceece596f5d5c602a5c6b
simchri <simon.massa@hotmail.de>
2025-01-19 21:47:33 +0100

    move return to bottom

--- apps/calc/src/main.cpp
+++ apps/calc/src/main.cpp
@@ -2,7 +2,6 @@
 #include "ftxui/component/component_base.hpp"     // for ComponentBase
 #include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
 #include "ftxui/dom/elements.hpp" // for Elements, Element, operator|, separator, text, focusPositionRelative, size, border, flex, frame, bgcolor, gridbox, vbox, EQUAL, center, HEIGHT, WIDTH
-#include "ftxui/dom/elements.hpp" // for Elements, Element, operator|, separator, text, focusPositionRelative, size, border, flex, frame, bgcolor, gridbox, vbox, EQUAL, center, HEIGHT, WIDTH
 #include "ftxui/screen/color.hpp" // for Color
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
@@ -14,17 +13,18 @@ import view;
 using namespace ftxui;
 
 Element make_history_box(const view::ViewModel& vm) {
-    return window(text("results"),
-    vbox(
-            // Render each line's input field.
-            [&]() -> std::vector<Element> {
-                std::vector<Element> elements;
-                elements.reserve(vm.history.size());
-                for (auto& history_item : vm.history) {
-                    elements.push_back(paragraph(history_item));
-                }
-                return elements;
-            }()));
+    auto box = window(text("results"),
+                      vbox(
+                              // Render each line's input field.
+                              [&]() -> std::vector<Element> {
+                                  std::vector<Element> elements;
+                                  elements.reserve(vm.history.size());
+                                  for (auto& history_item : vm.history) {
+                                      elements.push_back(paragraph(history_item));
+                                  }
+                                  return elements;
+                              }()));
+    return box;
 };
 
 
